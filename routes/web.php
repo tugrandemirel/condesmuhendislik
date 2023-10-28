1c0207755a18ffb75b6401a4b335cc621e323053
Tuğran Demirel <49235236+tugranDemirel@users.noreply.github.com>
2023-10-28 16:12:42 +0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')->as('admin.')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
