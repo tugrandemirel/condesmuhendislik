858188b0b6d5a166a76ced2ec9feee1d481125a4
Tuğran Demirel <49235236+tugranDemirel@users.noreply.github.com>
2023-10-30 04:24:01 +0300

    wip

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,133 +1,77 @@
 <!--footer-area start-->
-<footer class="footer-area heding-bg">
-    <div class="footer-bg-one pt-100 pb-70">
-        <img class="shapes__1" src="{{ asset('assets/front/img/shape/star-5b.svg') }}" alt="">
+<footer class="footer-area">
+    <div class="footer-bg-two pt-95 pb-50" data-background="{{ asset($_siteSetting->footer_image ?? 'assets/front/img/footer/footer-bg-1a.jpg') }}">
+        <img class="shapes__1" src="{{ asset('assets/front/img/shape/star-3a.svg') }}" alt="">
         <img class="shapes__2" src="{{ asset('assets/front/img/shape/line-7a.svg') }}" alt="">
-        <img class="shapes__4" src="{{ asset('assets/front/img/footer/footer-parts-1a.png') }}" alt="">
         <div class="blur__shape"></div>
-        <div class="big-style-text">Footer</div>
-        <div class="container">
-            <div class="row pb-50">
-                <div class="col-l2 text-lg-start text-center">
-                    <div class="footer__widget mb-30">
-                        <a href="{{ route('index') }}"><img src="{{ asset('assets/front/img/logo/header-logo-1.png') }}" alt="Logo"></a>
+        <div class="container pt-200 pt-lg-10">
+            <div class="row mb-25">
+                <div class="col-xxl-3 col-lg-4 col-md-6">
+                    <div class="footer__widget mb-30 px-xxl-3">
+                        <a href="{{ route('index') }}"><img src="{{ asset($_siteSetting->logo) }}" width="217" height="62" alt="{{ $_siteSetting->title }}"></a>
+                        <p class="footer__description mt-40">  “Mühendislik'te yenilik, Condes'te evrensel standart!”</p>
                     </div>
                 </div>
-            </div>
-            <div class="row pb-35">
-                <div class="col-lg-3 col-md-6">
+                <div class="col-xxl-3 col-lg-4 col-md-6">
                     <div class="footer__widget mb-30">
-                        <div class="footer__contact__one">
-                            <div class="icon mb-35"><img src="{{ asset('assets/front/img/icon/icon-49d.svg') }}" alt="icon"></div>
-                            <h4 class="widget__title mb-30">Email</h4>
-                            <h5 class="widget__info">Get in touch</h5>
-                            <a class="widget__query" href="mailto:factry@example.com">factry@example.com</a>
-                        </div>
+                        <ul class="fot-list">
+                            <li>
+                                <a href="{{ route('index') }}">Anasayfa</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('about') }}">Hakkımızda</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('service') }}">Hizmetlerimiz</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('blog') }}">Makalelerimiz</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('contact') }}">İletişim</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6">
+                <div class="col-xxl-3 col-lg-4 col-md-6">
                     <div class="footer__widget mb-30">
-                        <div class="footer__contact__one">
-                            <div class="icon mb-35"><img src="{{ asset('assets/front/img/icon/icon-50d.svg') }}" alt="icon"></div>
-                            <h4 class="widget__title mb-30">Phone</h4>
-                            <h5 class="widget__info">Call or text</h5>
-                            <a class="widget__query" href="#">(603) 555-0123</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer__widget mb-30">
-                        <div class="footer__contact__one">
-                            <div class="icon mb-35"><img src="{{ asset('assets/front/img/icon/icon-51d.svg') }}" alt="icon"></div>
-                            <h4 class="widget__title mb-30">Office</h4>
-                            <a class="widget__query" href="#">4140 Parker Rd. Allentown, New Mexico 31134</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer__widget mb-30">
-                        <div class="footer__contact__one">
-                            <h4 class="widget__title mb-25">Hızlı Erişim</h4>
-                            <ul class="fot-list">
-                                <li>
-                                    <a href="{{ route('index') }}">Anasayfa</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('about') }}">Hakkımızda</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('service') }}">Hizmetlerimiz</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('blog') }}">Makalelerimiz</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('contact') }}">İletişim</a>
-                                </li>
-                            </ul>
+                        <div class="footer__contact">
+
+                            @if(!is_null($_siteSetting->phone) || !is_null($_siteSetting->phone_2))
+                                @if($_siteSetting->phone)
+                                    <h4 class="phone__nimber mb-25"><a href="tel:{{ $_siteSetting->phone }}">{{ $_siteSetting->phone }}</a></h4>
+                                @endif
+                                @if($_siteSetting->phone_2)
+                                    <h4 class="phone__nimber mb-25"><a href="tel:{{ $_siteSetting->phone_2 }}">{{ $_siteSetting->phone_2 }}</a></h4>
+                                @endif
+                            @endif
+                            @if(!is_null($_siteSetting->email))
+                                    <p class="mb-0"><a href="mailto:{{ $_siteSetting->email }}">{{ $_siteSetting->email }}</a></p>
+                                @endif
+                                @if(!is_null($_siteSetting->address))
+                            <p class="mb-0">{{ $_siteSetting->address }}</p>
+                                @endif
+                            <p class="mb-0">P.tesi: 08.00-18.00</p>
+                            <p class="mb-0">C.tesi: 08.00-18.00</p>
+
                         </div>
                     </div>
                 </div>
-                <div class="row mb-25 d-none">
-                    <div class="col-xxl-3 col-lg-4 col-md-6">
-                        <div class="footer__widget mb-30 px-xxl-3">
-                            <a href="{{ route('index') }}"><img src="{{ asset('assets/front/img/logo/header-logo-1.png') }}" alt="Logo"></a>
-                            <p class="footer__description mt-40">
-                                Rapidiously unleash best of the breed
-                                meta-services
-                                after
-                                principled vortals.</p>
-                        </div>
-                    </div>
-                    <div class="col-xxl-3 col-lg-4 col-md-6">
-                        <div class="footer__widget mb-30">
-                            <ul class="fot-list">
-                                <li>
-                                    <a href="faq.html">About Us</a>
-                                </li>
-                                <li>
-                                    <a href="faq.html">Products & Services</a>
-                                </li>
-                                <li>
-                                    <a href="faq.html">Manufacturing Process</a>
-                                </li>
-                                <li>
-                                    <a href="contact.html">Research Development</a>
-                                </li>
-                                <li>
-                                    <a href="contact.html">Case Studies</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xxl-3 col-lg-4 col-md-6">
-                        <div class="footer__widget mb-30">
-                            <div class="footer__contact">
-                                <h3 class="phone__nimber mb-25">+80 555 20103</h3>
-                                <p class="mb-0">4517 Washington Ave.</p>
-                                <p class="mb-35">Manchester, Kentucky 39495</p>
-                                <div class="social_media">
-                                    <a href="#">
-                                        <i class="fab fa-facebook-f"></i>
-                                    </a>
-                                    <a href="#">
-                                        <i class="fab fa-twitter"></i>
-                                    </a>
-                                    <a href="#">
-                                        <i class="fab fa-linkedin-in"></i>
-                                    </a>
-                                    <a href="#">
-                                        <i class="fab fa-youtube"></i>
-                                    </a>
-                                </div>
-                            </div>
+                <div class="col-xxl-3 col-lg-6 col-md-6">
+                    <div class="footer__widget mb-30">
+                        <div class="social_media">
+                           @foreach($_socialMedia as $_social)
+                            <a href="{{ $_social->url }}" target="_blank" title="{{ $_social->name }}">
+                                {!! $_social->icon !!}
+                            </a>
+                            @endforeach
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="copyright__border pt-75">
-            <div class="container">
+        <div class="container copyright__border">
+            <div class="copyright__area pt-75">
                 <div class="row align-items-center">
                     <div class="col-lg-6 text-center text-lg-start">
                         <div class="copyright__text mb-30">
@@ -136,9 +80,8 @@
                                 © 2023 Condes Mühendislik. Tüm hakları saklıdır.</p>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
+    </div>
 </footer>
-<!--footer-area end-->
